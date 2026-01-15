d9f3f815fc193d44545fe44acd27ba6709dd1910
William Blake Galbreath <Blake.Galbreath@GMail.com>
2026-01-15 16:03:27 -0600

    cleanup formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    `java-library`
+  `java-library`
 }
 
 group = "net.pl3x.livemap"
@@ -7,30 +7,30 @@ version = "1.0.0-SNAPSHOT"
 description = "LiveMap for Hytale servers"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    // todo - replace with maven repo when available
-    implementation(files(System.getenv("HYTALE_SERVER")))
+  // todo - replace with maven repo when available
+  implementation(files(System.getenv("HYTALE_SERVER")))
 }
 
 tasks {
-    processResources {
-        filteringCharset = Charsets.UTF_8.name()
+  processResources {
+    filteringCharset = Charsets.UTF_8.name()
 
-        // work around IDEA-296490
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
-        with(copySpec {
-            include("manifest.json")
-            from("src/main/resources") {
-                expand(
-                    "artifact" to "${rootProject.name}",
-                    "group" to "${rootProject.group}",
-                    "version" to "${rootProject.version}",
-                    "description" to "${rootProject.description}",
-                )
-            }
-        })
-    }
+    // work around IDEA-296490
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+    with(copySpec {
+      include("manifest.json", "web/index.html")
+      from("src/main/resources") {
+        expand(
+          "artifact" to "${rootProject.name}",
+          "group" to "${rootProject.group}",
+          "version" to "${rootProject.version}",
+          "description" to "${rootProject.description}",
+        )
+      }
+    })
+  }
 }
